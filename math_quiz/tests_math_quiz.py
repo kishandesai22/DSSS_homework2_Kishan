d99ea9aca8692fd02eb7ab7665bea1d2115c428f
kishandesai22 <151087266+kishandesai22@users.noreply.github.com>
2023-11-24 14:53:21 +0100

    Updated and added unit test cases tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -14,17 +14,25 @@ class TestMathGame(unittest.TestCase):
 
     def test_function_B(self):
         # TODO
+        operators = ['+', '-', '*', '/']
+        for _ in range(1000):  # Test a large number of random values
+            rand_operator = function_B()
+            self.assertIn(rand_operator, operators)
         pass
 
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+                (8, 3, '-', '8 - 3', 5),
+            (4, 6, '*', '4 * 6', 24),
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            for num1, num2, operator, expected_problem, expected_answer in
+test_cases:
                 # TODO
-                pass
+                result = function_C(num1, num2, operator)
+                self.assertEqual(result, (expected_problem,expected_answer))
+            pass
 
 if __name__ == "__main__":
     unittest.main()
