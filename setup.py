ecf74cde3e0c1a3fcecb0cfff2cf953a10c50099
kishandesai22 <151087266+kishandesai22@users.noreply.github.com>
2023-11-24 15:11:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name="snowflake",
+    name="Kishan",
     version="0.1",
-    author="DSSS",
-    author_email="luisa.e.neubig@fau.de",
+    author="KD",
+    author_email="kishan.desai@fau.de",
     packages=find_packages(),
     install_requires=["numpy", "turtles"],
 )
